a18aaa778c43209f6badbff6c084f938d60f0651
Cole <league@WTS-IM-17.attlocal.net>
2017-08-20 15:59:09 -0700

    08/20/17

---
+++ src/Ideas/Fps.java
@@ -0,0 +1,11 @@
+package Ideas;
+
+public class Fps {
+	//A man trying to take down people trying to invade a place
+	//viewed from the side
+	//wall to the right of the screen
+	//people running and trying to climb the wall
+	//side view - sniper with scope
+	// buttons: space/shoot - updownleftright/move - z/scope
+	//
+}
